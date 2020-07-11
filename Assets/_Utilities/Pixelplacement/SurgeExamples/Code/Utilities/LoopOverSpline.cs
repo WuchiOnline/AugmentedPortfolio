3b402b2b876d2f770e16b262108f06adf1487f0e
Eric Wu <eric@wuchi.online>
2020-07-11 16:48:34 -0700

    Added MVP Assets, Utilities, and Plugins. Changed color space to Linear.

---
+++ Assets/_Utilities/Pixelplacement/SurgeExamples/Code/Utilities/LoopOverSpline.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable 0649
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixelplacement;
+
+namespace Pixelplacement
+{
+    public class LoopOverSpline : MonoBehaviour
+    {
+        //Private Methods:
+        [SerializeField] Spline _spline;
+        [SerializeField] float _duration = 7;
+
+        //Init:
+        void Awake ()
+        {
+            Tween.Spline (_spline, transform, 0, 1, true, _duration, 0, Tween.EaseLinear, Tween.LoopType.Loop);
+        }
+    }
+}
