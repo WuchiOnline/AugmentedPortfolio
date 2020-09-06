944b86fd1d1b1c242b918915f9875d09b63695f9
Eric Wu <eric@wuchi.online>
2020-09-06 16:20:17 -0700

    Setup new lab Scene with UI overlay example. Waiting for TMPro fix from BetterUI support.

---
+++ Assets/_Utilities/TheraBytes/BetterUI/Editor/Scripts/UiLayout/TransformScalerEditor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace TheraBytes.BetterUi.Editor
+{
+    [CustomEditor(typeof(TransformScaler))]
+    public class TransformScalerEditor : UnityEditor.Editor
+    {
+        SerializedProperty scaleSizerFallback, scaleSizerCollection;
+
+        void OnEnable()
+        {
+            scaleSizerFallback = serializedObject.FindProperty("scaleSizerFallback");
+            scaleSizerCollection = serializedObject.FindProperty("customScaleSizers");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            ScreenConfigConnectionHelper.DrawSizerGui("Scale Settings", scaleSizerCollection, ref scaleSizerFallback);
+        }
+    }
+}
---
+++ Assets/_Utilities/TheraBytes/BetterUI/Runtime/Scripts/BetterUiLayout/TransformScaler.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace TheraBytes.BetterUi
+{
+    [AddComponentMenu("Better UI/Helpers/Transform Scaler", 30)]
+    public class TransformScaler : ResolutionSizer<Vector3>
+    {
+        public Vector3SizeModifier ScaleSizer { get { return customScaleSizers.GetCurrentItem(scaleSizerFallback); } }
+
+        protected override ScreenDependentSize<Vector3> sizer { get { return customScaleSizers.GetCurrentItem(scaleSizerFallback); } }
+
+        [FormerlySerializedAs("scaleSizer")]
+        [SerializeField]
+        Vector3SizeModifier scaleSizerFallback = new Vector3SizeModifier(Vector3.one, Vector3.zero, 4 * Vector3.one);
+
+        [SerializeField]
+        Vector3SizeConfigCollection customScaleSizers = new Vector3SizeConfigCollection();
+
+        DrivenRectTransformTracker rectTransformTracker = new DrivenRectTransformTracker();
+
+
+        void OnDisable()
+        {
+            rectTransformTracker.Clear();
+        }
+
+        protected override void ApplySize(Vector3 newSize)
+        {
+            rectTransformTracker.Clear();
+
+            RectTransform rt = this.transform as RectTransform;
+            if(rt != null)
+            {
+                rectTransformTracker.Clear();
+                rectTransformTracker.Add(this, rt, DrivenTransformProperties.Scale);
+            }
+
+            this.transform.localScale = newSize;
+        }
+    }
+}
